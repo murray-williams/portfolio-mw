8e0d18b9752b80e970f11d188c2b55c90b750a35
Murray Williams <murray@waterfalldigital.co.za>
2021-11-09 14:45:24 +0200

    Setup scss

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <BaseLayout>
       <Container>
-        <h1>I am index page</h1>
         <Button color="danger">Danger</Button>
       </Container>
     </BaseLayout>
