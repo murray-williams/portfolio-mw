f8beac5564aac138ebbd5221de9b1146f2f6f20d
Murray Williams <murray@waterfalldigital.co.za>
2021-11-10 11:28:20 +0200

    Handle loading state with SWR

--- actions/index.js
+++ actions/index.js
@@ -1,6 +1,16 @@
 import useSWR from 'swr'
-const fetcher = (url) => fetch(url).then((res) => res.json())
+const fetcher = (url) =>
+  fetch(url).then(async (res) => {
+    const result = await res.json()
+
+    if (res.status !== 200) {
+      return Promise.reject(result)
+    } else {
+      return result
+    }
+  })
 
 export const useGetPosts = () => {
-  return useSWR('/api/v1/posts', fetcher)
+  const { data, error, ...rest } = useSWR('/api/v1/posts', fetcher)
+  return { data, error, loading: !data && !error, ...rest }
 }
